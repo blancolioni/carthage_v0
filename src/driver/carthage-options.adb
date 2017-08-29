488d1c58fd9670b61ef8ab0aa1a91bbe57e246ee
Fraser Wilson <blancolioni@gmail.com>
2017-08-29 10:54:47 +0200

    New options: --trace-planet-import and --trace-unit-import

--- src/driver/carthage-options.adb
+++ src/driver/carthage-options.adb
@@ -10,4 +10,16 @@ package body Carthage.Options is
                ("clear-import-cache", ' ');
    end Clear_Import_Cache;
 
+   function Trace_Planet_Import return Boolean is
+   begin
+      return WL.Command_Line.Find_Option
+               ("trace-planet-import", ' ');
+   end Trace_Planet_Import;
+
+   function Trace_Unit_Import return Boolean is
+   begin
+      return WL.Command_Line.Find_Option
+               ("trace-unit-import", ' ');
+   end Trace_Unit_Import;
+
 end Carthage.Options;
--- src/driver/carthage-options.ads
+++ src/driver/carthage-options.ads
@@ -1,5 +1,7 @@
 package Carthage.Options is
 
    function Clear_Import_Cache return Boolean;
+   function Trace_Planet_Import return Boolean;
+   function Trace_Unit_Import return Boolean;
 
 end Carthage.Options;
